b8e114095591ffbe7de05a70af91aa12883f4148
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-15 20:20:49 -0300

    update 202411152020

--- include/st25vf010/st25vf010.hpp
+++ include/st25vf010/st25vf010.hpp
@@ -4,10 +4,10 @@
 #define SECTOR_ERASE_CMD 0x20 // Comando para borrar un sector
 #define SECTOR_SIZE 4096      // Tamaño de cada sector (en bytes)
 
-//#define ADDRESS_END         0x1FFFF
+#define ADDRESS_END         0x1FFFF
 //#define MEMORY_SIZE ADDRESS_END
 //#define MEMORY_SIZE (32 * 1024) // Tama      o de la memoria en bytes (32 KB)
-#define MEMORY_SIZE 131072    // Tamaño total de la memoria (en bytes)
+//#define MEMORY_SIZE 131072    // Tamaño total de la memoria (en bytes)
 
 namespace SPI{
     struct Spi_t ;
