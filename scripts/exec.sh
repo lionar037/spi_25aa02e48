3dedc3230962dc076e2dd086dfe570e0c2339b48
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-16 13:43:38 -0300

    update 202411161343

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -6,6 +6,8 @@ os_name=$(uname)
 # Detectar la arquitectura (32 o 64 bits)
 if [ "$(uname -m)" == "x86_64" ]; then
     arch="64 bits"
+elif [[ "$(uname -m)" == aarch64 ]]; then
+    arch="ARM de 64 bits"    
 elif [[ "$(uname -m)" == arm* || "$(uname -m)" == aarch32 ]]; then
     arch="ARM de 32 bits"
 else
@@ -34,9 +36,7 @@ arch=$(uname -m)
 
 if [ "$arch" == "aarch64" ]; then
     ./bin/st25vf010_app_$arch
-fi
-
-if [ "$arch" == "armv7l" ]; then
+elif [ "$arch" == "armv7l" ]; then
     ./bin/st25vf010_app_$arch
 elif [[ "$arch" == arm* ]]; then
     ./bin/st25vf010_app_$arch
