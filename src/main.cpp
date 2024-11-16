50aa676befbecc839577557b61a5431884943130
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-15 22:52:59 -0300

    update 202411152252

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,11 @@
 
 int main (){
 
-auto st25vf {std::make_unique<ST25VF010::St25vf010_t>()};
-
-
-//st25vf->read();
-//st25vf->erase();
-
-st25vf->write_rand();
-st25vf->read();
+    auto st25vf {std::make_unique<ST25VF010::St25vf010_t>()};
+    //st25vf->erase();
+    //st25vf->write_rand();
+    st25vf->write();
+    st25vf->read();
 
 return 0;    
 }
