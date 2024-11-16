5acdb4957ed8a6f00cdf31a4d15a817cbf9dc70b
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-16 20:24:02 -0300

    update 202411162024

--- src/sst25vf010/write_sst25vf.cpp
+++ src/sst25vf010/write_sst25vf.cpp
@@ -58,13 +58,13 @@ namespace ST25VF010{
 
 		uint32_t address=0x00;
 		//spi->writeEnable();
-		spi->write(address, vect_data);
+		spi->write_aai(address, vect_data);
 		address++;
 		for( ; address < ADDRESS_END ; ) 
 		//for( ; address < 0x1fff ; ) 
 			{				
 				TYME::delay_ms(1);
-				spi->write(address, vect_data);
+				spi->write_aai(address, vect_data);
 				address += vect_data.size();
 				
 			}
