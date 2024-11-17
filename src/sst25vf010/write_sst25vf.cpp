3436b97b6646de11c00ec6609448845cc3f6672c
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-16 22:02:00 -0300

    update 202411162202

--- src/sst25vf010/write_sst25vf.cpp
+++ src/sst25vf010/write_sst25vf.cpp
@@ -17,39 +17,37 @@ namespace ST25VF010{
 		//spi->read_status();
 	}
 
-	void St25vf010_t::write_rand() {	
-	init();
-		uint8_t buffer[4]={0xa1,0xbb,0xcc,0xdd};
-		for(uint32_t address=0x000000;address < 0x1ff;){
-				spi->write(address, buffer ,1);
-				buffer[0]=rand()%256;
-				address += 1;
-			}
-			spi->writeDisable();		
+		void St25vf010_t::write_rand() {	
+		//init();
+		//uint8_t buffer[4]={0xa1,0xbb,0xcc,0xdd};
+		//for(uint32_t address=0x000000;address < 0x1ff;){
+		//		spi->write(address, buffer ,1);
+		//		buffer[0]=rand()%256;
+		//		address += 1;
+		//	}
+		//	spi->writeDisable();		
 		}
 	
 
-	void St25vf010_t::write(const uint8_t data) {	
-		uint8_t buffer[4]={0xa1,0xbb,0xcc,0xdd};
-		init();
-		for(uint32_t address=0x000000;address < ADDRESS_END;){
-				spi->write(address, buffer ,1);				
-				buffer[0]=data;
-				address += 1;
-			}
-			spi->writeDisable();
+		void St25vf010_t::write(const uint8_t data) {	
+		//uint8_t buffer[4]={0xa1,0xbb,0xcc,0xdd};
+		//init();
+		//for(uint32_t address=0x000000;address < ADDRESS_END;){
+		//		spi->write(address, buffer ,1);				
+		//		buffer[0]=data;
+		//		address += 1;
+		//	}
+		//	spi->writeDisable();
 		}
 
-	void St25vf010_t::write() {	
-		std::vector <uint8_t>vect_data(1,0xAE);
-		init();
-		
-		for(uint32_t address=0x00 ; address < ADDRESS_END ; ) {
-				spi->write(address, vect_data);
-				address += vect_data.size();
-			}
-
-			spi->writeDisable();			
+		void St25vf010_t::write() {	
+		//std::vector <uint8_t>vect_data(1,0xAE);
+		//init();		
+		//for(uint32_t address=0x00 ; address < ADDRESS_END ; ) {
+		//		spi->write(address, vect_data);
+		//		address += vect_data.size();
+		//	}
+		//	spi->writeDisable();			
 		}
 
 		void St25vf010_t::write_aai() {	
@@ -67,8 +65,8 @@ namespace ST25VF010{
 				spi->write_aai(address, vect_data);
 				address += vect_data.size();				
 			}
-		spi->cmd_byte_spi(WRDI);//1 byte de comando // Write Disable (WRDI) , Instruction to terminate , 
-		[[maybe_unused]] auto status = spi->cmd_byte_spi_duo(RDSR);//Read Status Register (RDSR)				
+		spi->cmd_byte_spi(SPIConstants::WRDI);//1 byte de comando // Write Disable (WRDI) , Instruction to terminate , 
+		[[maybe_unused]] auto status = spi->cmd_byte_spi_duo(SPIConstants::RDSR);//Read Status Register (RDSR)				
 		std::cout << "status : "<< std::to_string(status)<<"\n";
 		}
 	
