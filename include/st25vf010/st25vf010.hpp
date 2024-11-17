c7a4c95a08cdb6b01ad1950103ad116146792ffb
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-11-16 22:44:29 -0300

    update 202411162244

--- include/st25vf010/st25vf010.hpp
+++ include/st25vf010/st25vf010.hpp
@@ -7,11 +7,8 @@
 
 //#define SECTOR_ERASE_CMD    0x20 // Comando para borrar un sector
 //#define SECTOR_SIZE         4096      // Tamaño de cada sector (en bytes)
-
 //#define ADDRESS_END         0x1FFFF
-
 //#define MEMORY_SIZE         ADDRESS_END
-
 //#define MEMORY_SIZE (32 * 1024) // Tama      o de la memoria en bytes (32 KB)
 //#define MEMORY_SIZE 131072    // Tamaño total de la memoria (en bytes)
 
